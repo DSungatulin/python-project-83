21bc061f49634c3531f65240d23c00fba92ba88d
DSungatulin <sungatulindaniyar@gmail.com>
2024-04-11 05:59:27 +0600

    end of step 1

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/DSungatulin/python-project-83/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/DSungatulin/python-project-83/actions)
+https://python-project-83-poti.onrender.com
---
+++ page_analyzer/app.py
@@ -0,0 +1,13 @@
+import os
+from flask import Flask
+from dotenv import load_dotenv
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+load_dotenv()
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
