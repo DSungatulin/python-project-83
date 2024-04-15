9dae76a2bae688c9bc7c9a68cba26f8a04c699a3
DSungatulin <sungatulindaniyar@gmail.com>
2024-04-16 00:42:22 +0600

    end of step 3

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,15 +1,82 @@
 import os
-from flask import Flask
+import psycopg2
+import validators
+
+
 from dotenv import load_dotenv
-
-app = Flask(__name__)
-
-
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+from urllib.parse import urlparse
+from datetime import date
+from flask import (
+    Flask,
+    render_template,
+    request,
+    flash,
+    get_flashed_messages,
+    redirect,
+    url_for
+)
 
 
 load_dotenv()
+DATABASE_URL = os.getenv('DATABASE_URL')
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
+
+@app.route("/")
+def index_page():
+    return render_template('index.html')
+
+
+def normalize_data(item):
+    return [value if value else '' for value in item]
+
+
+@app.get('/urls')
+def render_add_new_page():
+    conn = psycopg2.connect(DATABASE_URL)
+    with conn.cursor() as cursor:
+        query = """
+            SELECT urls.id, urls.name, MAX(url_checks.created_at), MAX(status_code)
+            FROM urls
+            LEFT JOIN url_checks ON urls.id = url_checks.url_id
+            GROUP BY urls.id
+            ORDER BY urls.id DESC
+        """
+        cursor.execute(query)
+        urls = cursor.fetchall()
+        normalized_urls = [normalize_data(url) for url in urls]
+    return render_template('urls.html', urls=normalized_urls)
+
+
+@app.post('/urls')
+def add_new_page():
+    url = request.form.get('url', '')
+    url_max_len = 255
+    parsed_url = urlparse(url)
+    normalized_url = f"{parsed_url.scheme}://{parsed_url.hostname}"
+    conn = psycopg2.connect(DATABASE_URL)
+    with conn.cursor() as cursor:
+        cursor.execute('SELECT id FROM urls WHERE name=%s', (normalized_url,))
+        id = cursor.fetchone()
+
+        if not validators.url(url) or len(url) > url_max_len:
+            if len(url) > url_max_len:
+                flash('URL превышает максимально допустимую длину 255 символов', 'error')
+            else:
+                flash('Некорректный URL', 'error')
+            messages = get_flashed_messages(with_categories=True)
+            return render_template('index.html', messages=messages), 422
+
+        if not id:
+            cursor.execute(
+                "INSERT INTO urls (name, created_at) VALUES (%s, %s);",
+                (normalized_url, date.today()))
+            cursor.execute('SELECT id FROM urls WHERE name=%s', (normalized_url,))
+            id = cursor.fetchone()[0]
+            conn.commit()
+            flash('Страница добавлена', 'success')
+            return redirect(url_for('render_url_page', id=id))
+
+        flash('Страница уже существует', 'info')
+        return redirect(url_for('render_url_page', id=id[0]))
