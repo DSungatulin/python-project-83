d888a2f77b08707db9dfaac0fc55d49a185bf058
DSungatulin <sungatulindaniyar@gmail.com>
2024-04-25 20:17:53 +0600

    fix mentor's comments 2

--- page_analyzer/db.py
+++ page_analyzer/db.py
@@ -1,5 +1,7 @@
 from dotenv import load_dotenv
 from datetime import date
+# from urllib.parse import urlparse
+# from flask import request
 from page_analyzer.normalization import normalize_url
 
 
@@ -16,7 +18,8 @@ def connect_db():
     return conn
 
 
-def retrieve_page(conn):
+def retrieve_page():
+    conn = connect_db()
     with conn.cursor() as cursor:
         query = """
             SELECT urls.id, urls.name, MAX(url_checks.created_at), MAX(status_code)
@@ -30,16 +33,18 @@ def retrieve_page(conn):
         return urls
 
 
-def retrieve_id(conn):
+def retrieve_id():
+    conn = connect_db()
     with conn.cursor() as cursor:
         cursor.execute('SELECT id FROM urls WHERE name=%s', (normalize_url()[1],))
         id = cursor.fetchone()
         return id
 
 
-def check_db_data(conn):
+def check_db_data():
+    conn = connect_db()
     with conn.cursor() as cursor:
-        id = retrieve_id(conn)
+        id = retrieve_id()
         url = normalize_url()[1]
 
         cursor.execute(
@@ -51,37 +56,3 @@ def check_db_data(conn):
         id = cursor.fetchone()[0]
         conn.commit()
     return id
-
-
-def get_url_details(id, conn):
-    with conn.cursor() as cursor:
-        cursor.execute('SELECT name, created_at FROM urls WHERE id=%s', (id,))
-        return cursor.fetchone()
-
-
-def get_url_checks(id, conn):
-    with conn.cursor() as cursor:
-        cursor.execute("""SELECT id, status_code, h1, title, description, created_at
-                       FROM url_checks
-                       WHERE url_id=%s
-                       ORDER BY id DESC""",
-                       (id,))
-        return cursor.fetchall()
-
-
-def get_url_by_id(id, conn):
-    with conn.cursor() as cursor:
-        cursor.execute('SELECT name FROM urls WHERE id=%s', (id,))
-        return cursor.fetchone()[0]
-
-
-def insert_url_check(conn, id, status_code, h1, title, description):
-    conn.autocommit = True
-    with conn.cursor() as cursor:
-        cursor.execute(
-            """INSERT INTO url_checks
-            (url_id, status_code, h1, title, description, created_at)
-            VALUES (%s, %s, %s, %s, %s, %s);""",
-            (id, status_code, h1, title, description, date.today())
-        )
-    conn.commit()
