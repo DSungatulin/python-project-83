815862b044e05022ff1525187278956b4fda09f4
DSungatulin <sungatulindaniyar@gmail.com>
2024-04-26 12:13:47 +0600

    edit check_page

--- page_analyzer/db.py
+++ page_analyzer/db.py
@@ -72,11 +72,13 @@ def get_url_checks(id):
         checks = cursor.fetchall()
     return checks
 
+
 def get_url_by_id(id):
     conn = connect_db()
     with conn.cursor() as cursor:
         cursor.execute('SELECT name FROM urls WHERE id=%s', (id,))
         return cursor.fetchone()[0]
+
 
 def insert_url_check(id, status_code, h1, title, description):
     conn = connect_db()
