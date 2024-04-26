1d86eeb1dcb299f6b4107bc2aa230fa6655501fb
DSungatulin <sungatulindaniyar@gmail.com>
2024-04-26 21:11:39 +0600

    test commit

--- page_analyzer/db.py
+++ page_analyzer/db.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 from datetime import date
-from page_analyzer.normalization import normalize_url
+from urllib.parse import urlparse
+from flask import request
 
 
 import psycopg2
@@ -10,16 +11,21 @@ import os
 load_dotenv()
 
 
+def normalise_url():
+    url = request.form.get('url', '')
+    parsed_url = urlparse(url)
+    normalized_url = f"{parsed_url.scheme}://{parsed_url.hostname}"
+    return url, normalized_url
+
+
 def connect_db():
     DATABASE_URL = os.getenv('DATABASE_URL')
     conn = psycopg2.connect(DATABASE_URL)
     return conn
 
 
-conn = connect_db()
-
-
-def retrieve_page(conn):
+def retrieve_page():
+    conn = connect_db()
     with conn.cursor() as cursor:
         query = """
             SELECT urls.id, urls.name, MAX(url_checks.created_at), MAX(status_code)
@@ -33,54 +39,26 @@ def retrieve_page(conn):
         return urls
 
 
-def retrieve_id(conn):
+def retrieve_id():
+    conn = connect_db()
     with conn.cursor() as cursor:
-        cursor.execute('SELECT id FROM urls WHERE name=%s', (normalize_url()[1],))
+        cursor.execute('SELECT id FROM urls WHERE name=%s', (normalise_url()[1],))
         id = cursor.fetchone()
         return id
 
 
-def check_db_data(conn):
+def check_db_data():
+    conn = connect_db()
     with conn.cursor() as cursor:
-        id = retrieve_id(conn)
-        url = normalize_url()[1]
+        id = retrieve_id()
+        url = normalise_url()[1]
+
         cursor.execute(
             "INSERT INTO urls (name, created_at) VALUES (%s, %s);",
             (url, date.today())
         )
         cursor.execute('SELECT id FROM urls WHERE name=%s', (url,))
+
         id = cursor.fetchone()[0]
         conn.commit()
     return id
-
-
-def get_url_details(id, conn):
-    with conn.cursor() as cursor:
-        cursor.execute('SELECT name, created_at FROM urls WHERE id=%s', (id,))
-        url_details = cursor.fetchone()
-    return url_details
-
-
-def get_url_checks(id, conn):
-    with conn.cursor() as cursor:
-        cursor.execute("""SELECT id, status_code, h1, title, description, created_at
-                          FROM url_checks WHERE url_id=%s ORDER BY id DESC""", (id,))
-        checks = cursor.fetchall()
-    return checks
-
-
-def get_url_by_id(id, conn):
-    with conn.cursor() as cursor:
-        cursor.execute('SELECT name FROM urls WHERE id=%s', (id,))
-        return cursor.fetchone()[0]
-
-
-def insert_url_check(conn, id, status_code, h1, title, description):
-    conn.autocommit = True
-    with conn.cursor() as cursor:
-        cursor.execute(
-            """INSERT INTO url_checks
-            (url_id, status_code, h1, title, description, created_at)
-            VALUES (%s, %s, %s, %s, %s, %s);""",
-            (id, status_code, h1, title, description, date.today())
-        )
